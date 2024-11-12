c2661d706509d2d2d375e8284114dc70af7bf1fa
HoangDucBach <hoangbach0985@gmail.com>
2024-11-12 12:28:44 +0700

    add: review modal for user

--- app/(dashboard)/landmark/[id].tsx
+++ app/(dashboard)/landmark/[id].tsx
@@ -1,7 +1,8 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
-
+import { AntDesign, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { useLocalSearchParams } from "expo-router";
+
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { Area, Header, Main } from '@/components/screen';
 import { LandmarkContext } from '@/contexts/LandmarkContext';
@@ -9,16 +10,18 @@ import { Toast, ToastDescription, ToastTitle, useToast } from '@/components/ui/t
 import { Text } from '@/components/ui/text';
 import { Api } from '@/constants/Api';
 import { Image } from '@/components/ui/image';
+import { ScrollView } from '@/components/ui/scroll-view';
 import { Landmark, SpecialDish } from '@/types';
 import { VStack } from '@/components/ui/vstack';
-import { AntDesign, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
-import { Center } from '@/components/ui/center';
 import Field from '@/components/ui/field';
 import BottomToolbar from '@/components/screen/BottomToolbar';
 import { Button, ButtonText } from '@/components/ui/button';
-import { UserReviewCard } from '@/components/user';
-import { mockLandmarks, mockSpecialDishes } from '@/mock';
+import { UserReviewCard, UserReviewInput } from '@/components/user';
+import { mockLandmarkFeedbacks, mockLandmarks, mockSpecialDishes, mockUsers } from '@/mock';
 import { SpecialDishViewCard } from '@/components/special-dish';
+import { HStack } from '@/components/ui/hstack';
+import { ModalBody, ModalContent, ModalHeader, Modal, ModalBackdrop, ModalFooter } from '@/components/ui/modal';
+import { Heading } from '@/components/ui/heading';
 
 const AROUND_VIETNAM_API = Api.aroundvietnam.url;
 
@@ -27,7 +30,8 @@ export default function LandmarkDetailsScreen() {
   const toast = useToast();
   const [toastId, setToastId] = React.useState(0)
   const [landmark, setLandmark] = React.useState<Landmark | null>(null);
-  const [specialDishes, setSpecialDishes] = React.useState<SpecialDish []>([]);
+  const [specialDishes, setSpecialDishes] = React.useState<SpecialDish[]>([]);
+  const [showAllReviews, setShowAllReviews] = React.useState(false);
 
   const fetchLandmark = React.useCallback(async () => {
     try {
@@ -58,6 +62,66 @@ export default function LandmarkDetailsScreen() {
     }
   }, [id]);
 
+  const UserReviewArea = () => {
+    return (
+      <Area
+        title="Đánh giá"
+        more={
+          <Button variant='link' onPress={() => setShowAllReviews(true)}>
+            <ButtonText className='text-typography-500'>
+              Tất cả bình luận
+            </ButtonText>
+          </Button>
+        }
+      >
+        {mockLandmarkFeedbacks.slice(0, 5).map((feedback, index) => (
+          <UserReviewCard
+            key={index}
+            comment={feedback.comments}
+            rating={feedback.stars}
+            created_at={feedback.createdAt}
+            user={mockUsers[0]}
+          />
+        ))}
+        <UserReviewInput />
+        <Modal
+          isOpen={showAllReviews}
+          onClose={() => setShowAllReviews(false)}
+          size='full'
+          className='justify-end'
+        >
+          <ModalBackdrop />
+          <ModalContent className={'h-3/4'}>
+            <ModalHeader>
+              <VStack className='items-center w-full'>
+                <View style={styles.handle} />
+                <Heading size='lg' className='text-typography-900 text-lg'>
+                  Tất cả bình luận
+                </Heading>
+              </VStack>
+            </ModalHeader>
+            <ModalBody>
+              <ScrollView>
+                {mockLandmarkFeedbacks.map((feedback, index) => (
+                  <UserReviewCard
+                    key={index}
+                    comment={feedback.comments}
+                    rating={feedback.stars}
+                    created_at={feedback.createdAt}
+                    user={mockUsers[0]}
+                  />
+                ))}
+              </ScrollView>
+            </ModalBody>
+            <ModalFooter>
+              <UserReviewInput />
+            </ModalFooter>
+          </ModalContent>
+
+        </Modal>
+      </Area>
+    )
+  }
   React.useEffect(() => {
     if (id) {
       fetchLandmark();
@@ -117,32 +181,39 @@ export default function LandmarkDetailsScreen() {
           <Area
             title="Ẩm thực"
           >
-            {
-              specialDishes.map((dish, index) => (
-                <SpecialDishViewCard
-                  key={index}
-                  specialDish={dish}
-                />
-              ))
-            }
+            <ScrollView horizontal>
+              <HStack space='sm'>
+                {
+                  specialDishes.map((dish, index) => (
+                    <SpecialDishViewCard
+                      key={index}
+                      specialDish={dish}
+                    />
+                  ))
+                }
+              </HStack>
+            </ScrollView>
+
           </Area>
           <Area
             title="Vị trí"
           >
           </Area>
-          <Area
-            title="Đánh giá"
-          >
-            {/* <UserReviewCard
-              comment={''}
-              rating={0}
-              created_at={''}
-              user={{} as any}
-            /> */}
-          </Area>
+          <UserReviewArea />
 
         </Main>
       </ParallaxScrollView>
     </LandmarkContext.Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  handle: {
+    width: 40,
+    height: 5,
+    backgroundColor: '#ccc',
+    borderRadius: 2.5,
+    alignSelf: 'center',
+    marginVertical: 8,
+  },
+});
--- components/user/index.ts
+++ components/user/index.ts
@@ -1,1 +1,2 @@
 export * from './UserReviewCard'
+export * from './UserReviewInput'
