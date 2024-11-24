11334c5b4a0eb6cb71cf500fc823942fdc90ea11
HoangDucBach <hoangbach0985@gmail.com>
2024-11-24 19:57:24 +0700

    fix: open screen, update ui

--- app/(open)/_layout.tsx
+++ app/(open)/_layout.tsx
@@ -10,6 +10,8 @@ import { App } from '@/constants/App';
 import Carousel from 'react-native-reanimated-carousel';
 import { Box } from '@/components/ui/box';
 import { ThemedView } from '@/components/ThemedView';
+import { HStack } from '@/components/ui/hstack';
+import { Logo } from '@/components/icons';
 
 const coverImages = [
     require('@/assets/images/ha-long-bay.jpg'),
@@ -17,56 +19,61 @@ const coverImages = [
 ];
 
 export default function OpenLayout() {
+    const { width } = useWindowDimensions();
+    
     return (
         <VStack className='box-content w-full flex-1 p-4 justify-center' space='lg'>
-            <ThemedView className='flex-grow-[12] w-full relative'>
-                <Carousel
-                    style={{
-                        width: '100%',
-                        height: "100%",
-                        borderRadius: 24,
-                        shadowColor: '#000',
-                        shadowOffset: {
-                            width: 0,
-                            height: 2,
-                        },
-                        shadowOpacity: 0.25,
-                        shadowRadius: 3.84,
-                    }}
-                    width={640}
-                    data={[...coverImages.keys()]}
-                    autoPlay
-                    loop
-                    autoPlayInterval={5e3}
-                    renderItem={({ item }) => (
-                        <Image
-                            source={coverImages[item]}
-                            style={styles.image}
-                            resizeMode='cover'
-                        />
-                    )}
-                />
-                <Center className='absolute top-8 w-full'>
+            <VStack className='flex-grow-[12] w-full justify-between relative'>
+                <HStack className='z-50 px-4 py-8 items-center' space='sm'>
+                    <Logo size={32} />
+                    <Text size='2xl' className='text-white font-semibold capitalize'>{App.name}</Text>
+                </HStack>
+                <VStack className='absolute w-full h-full -z-50'>
+                    <Carousel
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            borderRadius: 24,
+                            shadowColor: '#000',
+                            shadowOffset: {
+                                width: 0,
+                                height: 2,
+                            },
+                            shadowOpacity: 0.25,
+                            shadowRadius: 3.84,
+                        }}
+                        width={640}
+                        data={[...coverImages.keys()]}
+                        autoPlay
+                        loop
+                        autoPlayInterval={5e3}
+                        renderItem={({ item }) => (
+                            <Image
+                                source={coverImages[item]}
+                                style={styles.image}
+                                resizeMode='cover'
+                            />
+                        )}
+                    />
+                </VStack>
+                <VStack className='w-full items-center justify-start p-4' space='md'>
+                    <Text size='3xl' className='text-white font-bold capitalize text-center'>{App.headline}</Text>
+                    <Text
+                        size='md'
+                        className='text-white font-light'
+                        style={{
+                            textAlign: 'center',
+                        }}
+                    >
+                        {App.welcomeText}
+                    </Text>
                     <Text
                         size='sm'
-                        className='font-light text-center bg-white/50 rounded-lg px-2 p-1 text-white w-fit '
+                        className='font-light text-center border border-white rounded-lg px-2 p-1 text-white w-fit '
                     >
-                        {App.version}
+                       Version {App.version}
                     </Text>
-                </Center>
-
-            </ThemedView>
-            <VStack className='flex-grow-[1] w-full items-center justify-start p-0' space='md'>
-                <Text size='4xl' className='text-typography-900 font-semibold capitalize text-center'>{App.name}</Text>
-                <Text
-                    size='md'
-                    className='text-typography-900 font-light'
-                    style={{
-                        textAlign: 'center',
-                    }}
-                >
-                    {App.welcomeText}
-                </Text>
+                </VStack>
             </VStack>
             <Box className='w-full flex-grow-[1]'>
                 <Slot />
