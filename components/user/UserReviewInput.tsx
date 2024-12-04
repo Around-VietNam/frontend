3b3d0a875209baa6bc9dc9e503c201294210d2e3
HoangDucBach <hoangbach0985@gmail.com>
2024-12-04 10:55:06 +0700

    update: user review input

--- components/user/UserReviewInput.tsx
+++ components/user/UserReviewInput.tsx
@@ -4,31 +4,42 @@ import { Avatar, AvatarFallbackText, AvatarImage } from "../ui/avatar";
 import { Input, InputField, } from "../ui/input";
 import React from "react";
 import { ScrollView } from "react-native";
+import { useAuth } from "@/hooks/useAuth";
+import { Button } from "../ui/button";
+import { Ionicons } from "@expo/vector-icons";
 
 interface Props extends ViewProps {
     // Add props here
 }
 export function UserReviewInput(props: Props) {
+    const { account } = useAuth();
+    const [avatarSrc, setAvatarSrc] = React.useState<string | undefined>(account?.avatar_url);
+
     const scrollViewRef = React.useRef<ScrollView>(null);
-    const handleFocus = () => {
-        scrollViewRef.current?.scrollToEnd({ animated: true });
-    };
+
+    React.useEffect(() => {
+        if (account?.avatar_url) {
+            setAvatarSrc(account.avatar_url);
+        }
+    }, [account]);
 
     return (
-        <HStack className="w-full py-4 items-center">
+        <HStack className="w-full p-4 items-center">
             <Avatar size="sm">
-                <AvatarFallbackText>
-                    N/A
-                </AvatarFallbackText>
-                <AvatarImage />
+                <AvatarImage source={{
+                    uri: avatarSrc,
+                }} />
             </Avatar>
             <Input
                 variant="rounded"
-                className="w-full border-0"
+                className="flex-1 border-0"
                 {...props}
             >
                 <InputField placeholder="Viết đánh giá ..." className="w-full" />
             </Input>
+            <Button onPress={() => { /* handle send action */ }} className="p-2 h-fit w-fit" size="lg">
+                <Ionicons name="send" size={16} color="white" />
+            </Button>
         </HStack>
     );
 }
